4bf0ec58b808fd9ea1ca818ac273e35d00703391
Antra Phukan <69572448+antrap@users.noreply.github.com>
2022-06-29 19:19:22 +0530

    Create Kth element in Matrix.cpp

---
+++ Heaps/Kth element in Matrix.cpp
@@ -0,0 +1,14 @@
+int kthSmallest(int mat[MAX][MAX], int n, int k)
+{
+  //Your code here
+  priority_queue<int,vector<int>,greater<int>> pq;
+  for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+          pq.push(mat[i][j]);
+      }
+  }
+  for(int i=0;i<k-1;i++){
+      pq.pop();
+  }
+  return pq.top();
+}
